1a296f70cb0714bb8307751f846f1f8b57dba9fd
Raphael Guye <raphael.guye@gmail.com>
2021-11-12 11:26:26 +0100

    Initial Commit

---
+++ EnvironmentObjectDemo/EnvironmentObjectDemoApp.swift
@@ -0,0 +1,15 @@
+//
+// Created by Raphael Guye on 12.11.21
+// https://github.com/raphaelguye
+// 
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
